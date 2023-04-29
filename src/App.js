16f660ef5d1ab3413b57abf8ced751cd69b5e3a7
Sayedfarabi <sayedfarabi6099@gmail.com>
2023-04-29 19:03:47 +0600

    image crop and resize functionality done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ImageCropDialog from "./components/imageCropDialog/ImageCropDialog";
+
 
 function App() {
+
+  const [images, setImages] = useState([]);
+
+  const [selectedImages, setSelectedImages] = useState([]);
+
+
+  const onSelectFile = (event) => {
+    const selectedFiles = event.target.files;
+    const selectedFilesArray = Array.from(selectedFiles);
+    const imagesArray = selectedFilesArray.map((file) => {
+      return URL.createObjectURL(file);
+    });
+    const imagesObjectArray = imagesArray.map(imageURL => {
+      return {
+        imageURL
+      }
+    })
+    setImages(imagesObjectArray);
+    setSelectedImages((previousImages) => previousImages?.concat(imagesArray));
+    event.target.value = "";
+  };
+
+  const onCancel = () => {
+    setSelectedImages([]);
+  };
+
+  const setCroppedImageFor = (imageURL, crop, zoom, aspect, croppedImageUrl) => {
+    const newImageList = [...images];
+    const imageIndex = images.findIndex((x) => x.imageURL === imageURL);
+    const image = images[imageIndex];
+    const newImage = { ...image, croppedImageUrl, crop, zoom, aspect };
+    newImageList[imageIndex] = newImage;
+    setImages(newImageList);
+    const filterSelectedImages = selectedImages.filter(image => image !== imageURL);
+    setSelectedImages(filterSelectedImages);
+  };
+
+  const resetImage = (id) => {
+    setCroppedImageFor(id);
+  };
+
+  function deleteHandler(image) {
+    // console.log(image);
+    setImages(images?.filter((e) => e.croppedImageUrl !== image));
+    // URL.revokeObjectURL(image);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="my-12 md:my-16">
+        <label className="mx-auto flex flex-col justify-center items-center border border-lime-500 text-blue-400 rounded-md w-24 h-24 text-center cursor-pointer">
+          + Add Images
+          <br />
+          <input
+            type="file"
+            name="images"
+            onChange={onSelectFile}
+            multiple
+            accept="image/png , image/jpeg, image/webp"
+            className="hidden"
+          />
+        </label>
+      </div>
+
+      <div className="py-8 md:py-12 grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
+        {
+          selectedImages?.length ?
+            selectedImages.map(image => {
+              return <ImageCropDialog
+                key={image}
+                imageURL={image}
+                onCancel={onCancel}
+                setCroppedImageFor={setCroppedImageFor}
+                resetImage={resetImage}
+              >
+
+              </ImageCropDialog>
+            })
+            :
+            null
+        }
+      </div>
+
+      <div className="w-full md:w-1/2 mx-4 md:mx-auto py-8 md:py-12">
+
+        {images.length ?
+          images.map((image, index) => {
+            return (
+              <div key={index} className="border-2 py-12 md:py-16 rounded-md">
+                <div className="flex justify-center items-center my-4">
+                  <img src={image?.croppedImageUrl} className=" rounded-md" alt="upload" />
+                </div>
+                <div className="text-center my-2">
+                  <button className="border px-2 bg-orange-500 text-white capitalize rounded-lg mx-2" onClick={() => deleteHandler(image?.croppedImageUrl)}>
+                    delete image
+                  </button>
+                  <a href={image?.croppedImageUrl} download={"download_img"}>
+                    <button className="border px-2 bg-blue-500 text-white capitalize rounded-lg mx-2">Download</button>
+                  </a>
+                </div>
+
+              </div>
+            );
+          })
+          :
+          null
+        }
+
+      </div>
     </div>
   );
 }
---
+++ src/components/imageCropDialog/ImageCropDialog.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import Cropper from 'react-easy-crop';
+import getCroppedImg from "../../cropImage";
+
+const aspectRatios = [
+  { value: 4 / 3, text: "4/3" },
+  { value: 16 / 9, text: "16/9" },
+  { value: 1 / 2, text: "1/2" },
+];
+
+const ImageCropDialog = ({ imageURL, onCancel, setCroppedImageFor, resetImage }) => {
+
+
+
+  const [zoom, setZoom] = useState(1);
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [aspect, setAspect] = useState(aspectRatios[0]);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+
+  const onCropChange = (crop) => {
+    setCrop(crop);
+  };
+
+  const onZoomChange = (zoom) => {
+    setZoom(zoom);
+  };
+
+  const onAspectChange = (e) => {
+    const value = e.target.value;
+    // console.log(value);
+    const ratio = aspectRatios.find((ratio) => ratio?.value == value);
+    // console.log(ratio);
+    setAspect(ratio);
+  };
+
+  const onCropComplete = (croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  };
+
+  const onCrop = async () => {
+    const croppedImageUrl = await getCroppedImg(imageURL, croppedAreaPixels);
+    setCroppedImageFor(imageURL, crop, zoom, aspect, croppedImageUrl);
+  };
+
+  const onResetImage = () => {
+    resetImage(imageURL);
+  };
+
+  return (
+    <div className='mx-auto'>
+
+      <div>
+        <Cropper
+          image={imageURL}
+          zoom={zoom}
+          crop={crop}
+          aspect={aspect?.value}
+          onCropChange={onCropChange}
+          onZoomChange={onZoomChange}
+          onCropComplete={onCropComplete}
+
+        >
+        </Cropper>
+      </div>
+
+      <div className="fixed bottom-0 h-20 w-full md:w-1/2 mx-4 md:mx-auto">
+        <div className="text-center">
+          <input
+            type="range"
+            min={1}
+            max={3}
+            step={0.1}
+            value={zoom}
+            onInput={(e) => {
+              onZoomChange(e?.target?.value);
+            }}
+            className="w-1/2"
+          ></input>
+          <select onChange={onAspectChange} className='px-4 mx-4 rounded-sm'>
+            {aspectRatios.map((ratio) => (
+              <option
+                key={ratio?.text}
+                value={ratio?.value}
+                selected={ratio?.value === aspect?.value}
+                className='text-blue-500'
+              >
+                {ratio?.text}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mt-5 text-center">
+          <button className=' mx-2 px-2 border bg-black text-white rounded-sm capitalize' onClick={onCancel}>Cancel</button>
+          <button className=' mx-2 px-2 border bg-black text-white rounded-sm capitalize' onClick={onResetImage}>Reset</button>
+          <button className=' mx-2 px-2 border bg-black text-white rounded-sm capitalize' onClick={onCrop}>Crop</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCropDialog;
